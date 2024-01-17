6f502da4635423773747d84050cf4173b8ea1d3b
Atefe <aa.asgariyan12@gmail.com>
2024-01-17 15:45:03 +0330

    create manula mode page

---
+++ src/app/pages/manual-mode-page/CameraViewBox.tsx
@@ -0,0 +1,22 @@
+type Props = {
+    targetComponentRef: React.MutableRefObject<null>;
+};
+
+const CameraViewBox: React.FC<Props> = ({ targetComponentRef }) => {
+  return (
+    <div className="col-lg-8 col-lx-9 p-0" ref={targetComponentRef}>
+      <div className="card  h-550px d-flex align-items-center justify-content-center">
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <img
+            className="w-100px"
+            src="media\img\camera-off.png"
+            alt="Camera Off"
+          />
+          <span className="fs-2 text-muted">Camera is off.</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CameraViewBox;
