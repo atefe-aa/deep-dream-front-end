a33980b29aad77344a0c35e8ffcd174a583592d0
Atefe <aa.asgariyan12@gmail.com>
2024-01-20 10:50:17 +0330

    make the filters checkbox like

--- src/app/ui/search-and-filter/FilterDropdown.tsx
+++ src/app/ui/search-and-filter/FilterDropdown.tsx
@@ -15,6 +15,25 @@ const FilterDropdown: React.FC<Props> = ({ filters = [] }) => {
     e.preventDefault();
     console.log(value);
   }
+
+  const [counsellors, setCounsellors] = useState<number[]>([]);
+
+  // function handleSubmit(e: React.FormEvent) {
+  //   e.preventDefault();
+  //   console.log(counsellors);
+  // }
+
+  function HandleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, checked } = e.target;
+
+    setCounsellors((prevCounsellors) => {
+      if (checked) {
+        return [...prevCounsellors, Number(name)];
+      } else {
+        return prevCounsellors.filter((item) => item !== Number(name));
+      }
+    });
+  }
   return (
     <div
       className="menu menu-sub menu-sub-dropdown w-250px w-md-300px"
@@ -30,8 +49,52 @@ const FilterDropdown: React.FC<Props> = ({ filters = [] }) => {
         {filters.find((filter) => filter === "lab") && (
           <div className="mb-10">
             <label className="form-label fw-bold">Laboratory:</label>
-
-            <div>
+            <div className="accordion" id={`accordion_filter`}>
+                <div className="accordion-item">
+                  <h2 className="accordion-header " id={`heading_filter`}>
+                    <button
+                      className="accordion-button h-30px collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target={`#collapse_filter`}
+                      aria-expanded="true"
+                      aria-controls={`collapse_filter`}
+                    >
+                      Laboratories List
+                    </button>
+                  </h2>
+                  <div
+                    id={`collapse_filter`}
+                    className="accordion-collapse collapse h-150px scroll-y"
+                    aria-labelledby={`heading_filter`}
+                    data-bs-parent={`#accordion_filter`}
+                  >
+                    <div className="accordion-body">
+                      {LABS_TESTS_DATA.map((lab) => (
+                        <div
+                          className="d-flex"
+                          key={lab.id}
+                          onClick={(e) => e.stopPropagation()}
+                        >
+                          <div className="form-check form-check-custom form-check-solid">
+                            <label className="form-label fw-bolder text-gray-800 fs-6">
+                              <input
+                                className="form-check-input me-3"
+                                type="checkbox"
+                                checked={counsellors.includes(lab.id)}
+                                onChange={HandleOnChange}
+                                name={lab.id.toString()}
+                              />
+                              {lab.labName}
+                            </label>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            {/* <div>
               <select
                 className="form-select "
                 data-kt-select2="true"
@@ -46,7 +109,7 @@ const FilterDropdown: React.FC<Props> = ({ filters = [] }) => {
                   </option>
                 ))}
               </select>
-            </div>
+            </div> */}
           </div>
         )}
 
@@ -54,14 +117,59 @@ const FilterDropdown: React.FC<Props> = ({ filters = [] }) => {
           <div className="mb-10">
             <label className="form-label fw-bold">Test Type:</label>
 
-            <select className="form-select " aria-label="Select test type">
+            <div className="accordion" id={`accordion_filter_test_type`}>
+                <div className="accordion-item">
+                  <h2 className="accordion-header " id={`heading_filter_test_type`}>
+                    <button
+                      className="accordion-button h-30px collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target={`#collapse_filter_test_type`}
+                      aria-expanded="true"
+                      aria-controls={`collapse_filter_test_type`}
+                    >
+                      Test Types List
+                    </button>
+                  </h2>
+                  <div
+                    id={`collapse_filter_test_type`}
+                    className="accordion-collapse collapse h-150px scroll-y"
+                    aria-labelledby={`heading_filter_test_type`}
+                    data-bs-parent={`#accordion_filter_test_type`}
+                  >
+                    <div className="accordion-body">
+                      {TEST_TYPES.map((test) => (
+                        <div
+                          className="d-flex"
+                          key={test.id}
+                          onClick={(e) => e.stopPropagation()}
+                        >
+                          <div className="form-check form-check-custom form-check-solid">
+                            <label className="form-label fw-bolder text-gray-800 fs-6">
+                              <input
+                                className="form-check-input me-3"
+                                type="checkbox"
+                                checked={counsellors.includes(test.id)}
+                                onChange={HandleOnChange}
+                                name={test.id.toString()}
+                              />
+                              {test.title}
+                            </label>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            {/* <select className="form-select " aria-label="Select test type">
               <option>Choose test type</option>
               {TEST_TYPES.map((test) => (
                 <option value={test.id} key={test.id}>
                   {test.code} - {test.title}
                 </option>
               ))}
-            </select>
+            </select> */}
           </div>
         )}
 
