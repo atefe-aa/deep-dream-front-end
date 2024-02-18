6d487c939c64a5655f1c450aaec9e7bfd2380ad8
Atefe <aa.asgariyan12@gmail.com>
2024-02-18 09:38:38 +0330

    replace registration word with admit in the table ui

--- src/app/modules/tests/Registrations.tsx
+++ src/app/modules/tests/Registrations.tsx
@@ -50,10 +50,10 @@ export function Registrations() {
   };
 
   const columns = [
-    "registration",
+    "admit number",
     "Patient",
     "National ID",
-    "registration Date & Time",
+    "admit Date & Time",
     "Sender Laboratory",
     "Progress",
   ];
@@ -61,7 +61,7 @@ export function Registrations() {
     <KTCardBody className="py-4">
       <Search updateState={searchFunction} />
       <div className="table-responsive">
-        <CustomTable tableTitle="Registrations List" className="" modalId="kt_modal_add_new_laboratory">
+        <CustomTable tableTitle="Admits List" className="" modalId="kt_modal_add_new_laboratory">
           <CustomTableHead>
             {columns.map((col) => (
               <CustomHeaderCell
