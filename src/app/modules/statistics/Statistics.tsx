99b32dd79e67e1d23d5a377d1851dfa938bea3b2
Atefe <aa.asgariyan12@gmail.com>
2024-02-21 16:49:58 +0330

    use the new structure for the statistics

--- src/app/modules/statistics/Statistics.tsx
+++ src/app/modules/statistics/Statistics.tsx
@@ -1,163 +1,160 @@
-import { hasRole } from "../../utils/helper";
+import React from "react";
 import { useAuth } from "../auth";
 import { BarChart } from "./components/BarChart";
 import { LineChart } from "./components/LineChart";
 import { RadarChart } from "./components/RadarChart";
 import { useChart } from "./hooks/useChart";
+import { hasRole } from "../../utils/helper";
+
+// Define a type for your chart configurations to improve type checking
+type ChartConfig = {
+  query: string;
+  chartType: "BarChart" | "LineChart" | "RadarChart"; // Specify exact string literals here
+  title: string;
+  unit: string;
+  color: string;
+  className: string;
+  chartHeight: string;
+  description: string;
+  change?: string | null;
+};
+
+const chartComponents = {
+  BarChart,
+  LineChart,
+  RadarChart,
+};
 
 export function Statistics() {
   const { currentUser } = useAuth();
-  const query1 = "x=laboratories&y=number";
-  const { isLoading, chartData } = useChart({ query: query1, chart: "chart" });
 
-  const query2 = "x=laboratories&y=price";
-  const { isLoading: isLoading2, chartData: chartData2 } = useChart({
-    query: query2,
-    chart: "chart",
-  });
-  const query3 = "x=testTypes&y=price";
-  const { isLoading: isLoading3, chartData: chartData3 } = useChart({
-    query: query3,
-    chart: "chart",
-  });
-  const query4 = "x=testTypes&y=number";
-  const { isLoading: isLoading4, chartData: chartData4 } = useChart({
-    query: query4,
-    chart: "chart",
-  });
-
-  const query5 =
-    "y=number&laboratories[]=13&laboratories[]=25&laboratories[]=36&laboratories[]=2&laboratories[]=5&laboratories[]=6&testTypes[]=1&testTypes[]=2&testTypes[]=3&testTypes[]=4&testTypes[]=5&testTypes[]=7r";
-  const { isLoading: isLoading5, chartData: chartData5 } = useChart({
-    query: query5,
-    chart: "radarChart",
-  });
-
-  const query6 =
-    "y=price&laboratories[]=13&laboratories[]=25&laboratories[]=36&laboratories[]=2&laboratories[]=5&laboratories[]=6&testTypes[]=1&testTypes[]=2&testTypes[]=3&testTypes[]=4&testTypes[]=5&testTypes[]=7r";
-  const { isLoading: isLoading6, chartData: chartData6 } = useChart({
-    query: query6,
-    chart: "radarChart",
-  });
+  const chartQueries: ChartConfig[] = [
+    {
+      query: "x=laboratories&y=number",
+      chartType: "BarChart",
+      title: "Tests Number",
+      className: "mb-5 mb-xl-8",
+      unit: " tests",
+      color: "info",
+      chartHeight: "365px",
+      description: "",
+    },
+    {
+      query: "x=laboratories&y=price",
+      chartType: "BarChart",
+      title: "Tests Price(R)",
+      unit: " (R)",
+      color: "primary",
+      className: " mb-xl-8",
+      chartHeight: "365px",
+      description: "",
+    },
+    {
+      query: "x=testTypes&y=price",
+      chartType: "LineChart",
+      title: "Test Types Prices(R)",
+      unit: " (R)",
+      color: "danger",
+      className: " mb-5 mb-xl-8",
+      description: "Test Types Prices(R)",
+      chartHeight: "",
+      change:
+        currentUser && hasRole(currentUser, "superAdmin")
+          ? "All Laboratories"
+          : currentUser?.data?.labName,
+    },
+    {
+      query: "x=testTypes&y=number",
+      chartType: "LineChart",
+      title: "Test Types Numbers",
+      unit: " Tests",
+      color: "success",
+      className: " mb-xl-8",
+      description: "Test Types Numbers",
+      change:
+        currentUser && hasRole(currentUser, "superAdmin")
+          ? "All Laboratories"
+          : currentUser?.data?.labName,
+      chartHeight: "",
+    },
+    {
+      query:
+        "y=number&laboratories[]=13&laboratories[]=25&laboratories[]=36&laboratories[]=2&laboratories[]=5&laboratories[]=6&testTypes[]=1&testTypes[]=2&testTypes[]=3&testTypes[]=4&testTypes[]=5&testTypes[]=7r",
+      chartType: "RadarChart",
+      title: "Test Number Base",
+      unit: " tests",
+      color: "success",
+      className: "mb-5 mb-xl-8 ",
+      description: "Price(R) Base",
+      chartHeight: "",
+    },
+    {
+      query:
+        "y=price&laboratories[]=13&laboratories[]=25&laboratories[]=36&laboratories[]=2&laboratories[]=5&laboratories[]=6&testTypes[]=1&testTypes[]=2&testTypes[]=3&testTypes[]=4&testTypes[]=5&testTypes[]=7r",
+      chartType: "RadarChart",
+      title: "Price(R) Base",
+      unit: " (R)",
+      color: "success",
+      className: " mb-xl-8",
+      description: "Test Number Base",
+      chartHeight: "",
+    },
+  ];
 
   return (
     <div className="row gy-5 g-xxl-8">
-      {/* begin::Col */}
-      {currentUser && hasRole(currentUser, "superAdmin") && (
-        <>
-          <div className="col-lg-6">
-            {isLoading ? (
-              <span>Loading....</span>
-            ) : (
-              <BarChart
-                totals={chartData.totals}
-                series={chartData.series}
-                chartTitle="Tests Number"
-                unit=" tests"
-                xaxisCategories={chartData.xAxisCategories}
-                className="mb-5 mb-xl-8"
-                color="info"
-                chartHeight="365px"
-              />
-            )}
-          </div>
-          <div className="col-lg-6">
-            {isLoading2 ? (
-              <span>Loading....</span>
-            ) : (
-              <BarChart
-                unit=" (R)"
-                totals={chartData2.totals}
-                series={chartData2.series}
-                chartTitle="Tests Price(R)"
-                xaxisCategories={chartData2.xAxisCategories}
-                className=" mb-xl-8"
-                color="primary"
-                chartHeight="365px"
-              />
-            )}
-          </div>
-        </>
+      {chartQueries.map(
+        (
+          {
+            query,
+            chartType,
+            title,
+            unit,
+            color,
+            className,
+            chartHeight,
+            description,
+            change,
+          },
+          index
+        ) => {
+          const chart = chartType === "RadarChart" ? "radarChart" : "chart";
+          const { isLoading, chartData } = useChart({ query, chart });
+
+          // Ensure ChartComponent cannot be undefined by asserting the type here
+          const ChartComponent = chartComponents[
+            chartType
+          ] as React.ElementType;
+
+          if (
+            chartType === "BarChart" &&
+            (!currentUser || !hasRole(currentUser, "superAdmin"))
+          ) {
+            return null;
+          }
+
+          return (
+            <div className="col-lg-6" key={index}>
+              {isLoading ? (
+                <span>Loading...</span>
+              ) : (
+                <ChartComponent
+                  totals={chartData.totals}
+                  series={chartData.series}
+                  chartTitle={title}
+                  unit={unit}
+                  xaxisCategories={chartData.xAxisCategories}
+                  className={className}
+                  color={color}
+                  chartHeight={chartHeight}
+                  change={change || ""}
+                  description={description || ""}
+                />
+              )}
+            </div>
+          );
+        }
       )}
-      <div className="col-lg-6">
-        {isLoading3 ? (
-          <span>Loading....</span>
-        ) : (
-          <LineChart
-            chartHeight=""
-            chartTitle=""
-            totals={chartData3.totals}
-            series={chartData3.series}
-            xaxisCategories={chartData3.xAxisCategories}
-            unit=" (R)"
-            className=" mb-5 mb-xl-8"
-            color="danger"
-            description="Test Types Prices(R)"
-            change={
-              currentUser && hasRole(currentUser, "superAdmin")
-                ? "All Laboratories"
-                : currentUser?.data?.labName
-            }
-          />
-        )}
-      </div>
-      <div className="col-lg-6">
-        {isLoading4 ? (
-          <span>Loading....</span>
-        ) : (
-          <LineChart
-            chartHeight=""
-            chartTitle=""
-            totals={chartData4.totals}
-            series={chartData4.series}
-            xaxisCategories={chartData4.xAxisCategories}
-            unit=" Tests"
-            className=" mb-xl-8"
-            color="success"
-            description="Test Types Numbers"
-            change={
-              currentUser && hasRole(currentUser, "superAdmin")
-                ? "All Laboratories"
-                : currentUser?.data?.labName
-            }
-          />
-        )}
-      </div>
-      {currentUser && hasRole(currentUser, "superAdmin") && (
-        <>
-          <div className="col-lg-6">
-            {isLoading5 ? (
-              <span>Loading....</span>
-            ) : (
-              <RadarChart
-                series={chartData5.series}
-                unit=" (R)"
-                className="mb-5 mb-xl-8 "
-                color="success"
-                description="Price(R) Base"
-                xaxisCategories={chartData5.xAxisCategories}
-                totals={chartData5.totals}
-              />
-            )}
-          </div>
-          <div className="col-lg-6">
-            {isLoading6 ? (
-              <span>Loading....</span>
-            ) : (
-              <RadarChart
-                series={chartData6.series}
-                unit=" tests"
-                className=" mb-xl-8 "
-                color="success"
-                description="Test Number Base"
-                xaxisCategories={chartData6.xAxisCategories}
-                totals={chartData6.totals}
-              />
-            )}
-          </div>
-        </>
-      )}
-      {/* end::Col */}
     </div>
   );
 }
--- src/app/pages/dashboard/DashboardWrapper.tsx
+++ src/app/pages/dashboard/DashboardWrapper.tsx
@@ -1,13 +1,11 @@
 import { Statistics } from "../../modules/statistics/Statistics";
-import { Statistics2 } from "../../modules/statistics/Statistics2";
 import { Registrations } from "../../modules/tests/Registrations";
 
 const DashboardWrapper = () => {
 
   return (
     <>
-      {/* <Statistics /> */}
-      <Statistics2 />
+      <Statistics />
       <Registrations />
     </>
   );
